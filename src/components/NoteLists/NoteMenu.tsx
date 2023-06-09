892de34f915c15f498a73dd96e837a1515afe5c2
saeful29 <saefulbarkah03@gmail.com>
2023-06-09 18:59:27 +0700

    fix: note list limit text title

--- src/components/NoteLists/NoteMenu.tsx
+++ src/components/NoteLists/NoteMenu.tsx
@@ -6,17 +6,14 @@ export const NoteMenu = () => {
     <div className="fixed top-0 left-0 bottom-0 ml-[305px] w-[350px] custom-scrollbar bg-foreColor/80">
       <div className="flex flex-col h-full my-[30px] px-[20px]">
         <h2 className="text-[22px] font-semibold">Personal</h2>
-        <div className="flex flex-col my-[30px] gap-[20px]">
+        <div className="flex flex-col pb-[30px] mt-[30px] gap-[20px]">
           {Array(5)
             .fill(null)
             .map((item, i) => (
               <Card className="bg-white/[3%] border-none hover:bg-white/[7%] transition cursor-pointer">
                 <CardContent className="p-[20px]">
-                  <h2 className="text-white truncate text-[18px] font-semibold">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Sequi dignissimos est qui. Ullam, nam facere id deleniti
-                    aliquam dicta reprehenderit rem perferendis pariatur quia
-                    totam, sunt voluptatum dolorem dolorum distinctio!
+                  <h2 className="text-white line-clamp-2 text-[18px] font-semibold leading-7">
+                    My Favorite Memories from Childhood
                   </h2>
                   <div className="flex gap-[10px] inactive-text mt-[10px]">
                     <p className="font-normal">31/12/2022</p>
