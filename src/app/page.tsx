207424c02853816374a63bc5e62a045def7d3873
saeful29 <saefulbarkah03@gmail.com>
2023-06-09 12:54:33 +0700

    add: select note to view page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Image from "next/image";
 const page: React.FC = () => {
-  return <div className="flex justify-center items-center h-screen">page</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col gap-[10px] items-center">
+        <Image
+          alt="icon"
+          priority
+          src={"/FileText.svg"}
+          height={80}
+          width={80}
+        />
+        <h2 className="font-semibold text-[28px]">Select note to view</h2>
+        <p className="w-[460px] text-center font-normal text-white/[60%]">
+          Choose a note from the list on the left to view its contents, or
+          create a new note to add to your collection.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
