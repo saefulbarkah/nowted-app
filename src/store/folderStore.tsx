fca10910af19db12199ca6ad8b9538ab90a4147d
saeful29 <saefulbarkah03@gmail.com>
2023-06-09 21:35:37 +0700

    remove: persistent storage folder

--- src/store/folderStore.tsx
+++ src/store/folderStore.tsx
@@ -13,74 +13,69 @@ export type folderStateType = {
   folders: {
     id: string | number;
     name: string;
+    can_delete?: boolean;
   }[];
   addFolder: (data: folderType) => void;
   editFolder: (editData: folderType) => void;
   deleteFolder: (id: string | number) => void;
 };
 
-export const useFolder = create<folderStateType>()(
-  persist(
-    (set) => ({
-      folders: [
-        {
-          id: uuid(),
-          name: "default folder",
-          notes_lists: null,
-        },
-      ],
-      addFolder: (data: folderType) => {
-        set((state) => {
-          const filterName = state.folders.filter((item) =>
-            item.name.includes(data.name)
-          );
-          const existingName = filterName[filterName.length - 1];
-          if (existingName) {
-            let uniqueName = `${data.name}_copy`;
-            const ranomizeNumber = Math.floor(Math.random() * 1000);
-            if (existingName.name.includes(uniqueName)) {
-              uniqueName = `${data.name}_copy ${ranomizeNumber}`;
-            }
-            return {
-              folders: [
-                ...state.folders,
-                {
-                  id: uuid(),
-                  name: uniqueName,
-                },
-              ],
-            };
-          }
-          return {
-            folders: [
-              ...state.folders,
-              {
-                id: uuid(),
-                name: data.name,
-              },
-            ],
-          };
-        });
-      },
-      editFolder: (data: folderType) => {
-        set((state) => {
-          const findData = state.folders.find((item) => item.id === data.id);
-          if (findData) {
-            findData.name = data.name;
-          }
-          return {
-            folders: [...state.folders],
-          };
-        });
-      },
-      deleteFolder: (id: string | number) => {
-        set((state) => ({
-          folders: state.folders.filter((item) => item.id !== id),
-        }));
-      },
-    }),
+export const useFolder = create<folderStateType>()((set) => ({
+  folders: [
     {
-      name: "folder-notes",
-    }
-  )
-);
+      id: uuid(),
+      name: "My folder",
+      notes_lists: null,
+      can_delete: false,
+    },
+  ],
+  addFolder: (data: folderType) => {
+    set((state) => {
+      const filterName = state.folders.filter((item) =>
+        item.name.includes(data.name)
+      );
+      const existingName = filterName[filterName.length - 1];
+      if (existingName) {
+        let uniqueName = `${data.name}_copy`;
+        const ranomizeNumber = Math.floor(Math.random() * 1000);
+        if (existingName.name.includes(uniqueName)) {
+          uniqueName = `${data.name}_copy ${ranomizeNumber}`;
+        }
+        return {
+          folders: [
+            ...state.folders,
+            {
+              id: uuid(),
+              name: uniqueName,
+            },
+          ],
+        };
+      }
+      return {
+        folders: [
+          ...state.folders,
+          {
+            id: uuid(),
+            name: data.name,
+          },
+        ],
+      };
+    });
+  },
+  editFolder: (data: folderType) => {
+    set((state) => {
+      const findData = state.folders.find((item) => item.id === data.id);
+      if (findData) {
+        findData.name = data.name;
+      }
+      return {
+        folders: [...state.folders],
+      };
+    });
+  },
+  deleteFolder: (id: string | number) => {
+    set((state) => ({
+      folders: state.folders.filter((item) => item.id !== id),
+    }));
+  },
+}));
