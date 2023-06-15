bee0c18d69791b36e47bb67a3f5039caf0dad0e3
saefulbarkah <saefulbarkah03@gmail.com>
2023-06-16 04:09:12 +0700

    add: loading state in button attribute

--- src/components/AuthPages/WithProvider.tsx
+++ src/components/AuthPages/WithProvider.tsx
@@ -1,38 +1,49 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { FcGoogle } from 'react-icons/fc';
 import { useRouter } from 'next/navigation';
+import { useToast } from '../ui/use-toast';
 
 function WithProvider() {
-   const supabase = createClientComponentClient();
-   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+  const { toast } = useToast();
 
-   async function handleSignGoogle() {
-      const { error } = await supabase.auth.signInWithOAuth({
-         provider: 'google',
-         options: {
-            redirectTo: `${location.origin}/auth/callback`
-         }
+  async function handleSignGoogle() {
+    setLoading(true);
+    try {
+      await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          redirectTo: `${location.origin}/auth/callback`,
+        },
       });
-      console.log(error);
-      if (!error) router.push('/note');
-   }
-   return (
-      <div className="flex flex-col gap-[20px]">
-         <Button
-            size={'lg'}
-            className="font-semibold border-white/[20%] capitalize"
-            variant={'outline'}
-            onClick={() => handleSignGoogle()}>
-            <span className="mr-2">
-               <FcGoogle className="text-[18px]" />
-            </span>
-            <span>Login With google</span>
-         </Button>
-      </div>
-   );
+      router.push('/note');
+    } catch (error) {
+      toast({
+        title: 'There was a problem',
+        description: 'There was an error loggin in with google',
+        variant: 'danger',
+      });
+    }
+  }
+  return (
+    <div className="flex flex-col gap-[20px]">
+      <Button
+        size={'lg'}
+        className="font-semibold border-white/[20%] capitalize"
+        variant={'outline'}
+        onClick={() => handleSignGoogle()}
+        isLoading={loading}
+      >
+        {loading ? null : <FcGoogle className="text-[18px] mr-2" />}
+        <span>Login With google</span>
+      </Button>
+    </div>
+  );
 }
 
 export default WithProvider;
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,32 +1,31 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-white/[5%] text-white hover:bg-white/[7%]",
-        ghost: "hover:bg-white/10 hover:text-white",
-        link: "underline-offset-4 hover:underline text-primary",
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline: 'border border-input hover:bg-accent hover:text-accent-foreground',
+        secondary: 'bg-white/[5%] text-white hover:bg-white/[7%]',
+        ghost: 'hover:bg-white/10 hover:text-white',
+        link: 'underline-offset-4 hover:underline text-primary',
       },
       size: {
-        default: "h-10 py-2 px-4",
-        sm: "h-9 px-3 rounded-md",
-        lg: "h-11 px-8 rounded-md",
+        default: 'h-10 py-2 px-4',
+        sm: 'h-9 px-3 rounded-md',
+        lg: 'h-11 px-8 rounded-md',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
   }
 );
@@ -34,21 +33,24 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  ({ className, variant, size, isLoading, children, ...props }, ref) => {
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={isLoading}
         {...props}
-      />
+      >
+        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {children}
+      </button>
     );
   }
 );
-Button.displayName = "Button";
+Button.displayName = 'Button';
 
 export { Button, buttonVariants };
