2ac44df6a38f9ab5404240cf4e66674dc8821745
saefulbarkah <saefulbarkah03@gmail.com>
2023-06-28 23:52:19 +0700

    fix: search notes

--- src/components/Sidebar/SearchNote.tsx
+++ src/components/Sidebar/SearchNote.tsx
@@ -8,7 +8,6 @@ import {
   AlertDialogContent,
   AlertDialogTrigger,
 } from '../ui/alert-dialog';
-import Link from 'next/link';
 import { FiFileText } from 'react-icons/fi';
 import useFolderState from '@/hooks/useFolderState';
 import { FolderTypes, NoteTypes } from '@/types';
@@ -16,10 +15,69 @@ import { useActiveNote } from '@/store/useActiveNote';
 import { useRouter } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
 import * as NProgress from 'nprogress';
+import { create } from 'zustand';
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog';
+
+interface TSearch {
+  search: string;
+  setSearch: (value: string) => void;
+  clearSearch: () => void;
+}
+
+interface Twait {
+  isWaiting: boolean;
+  setWaiting: (value: boolean) => void;
+}
+
+interface TLoading {
+  loading: Record<string | number, boolean>;
+  setLoading: (id: string | number) => void;
+  doneLoading: () => void;
+}
+
+const useSearchState = create<TSearch & Twait & TLoading>((set) => ({
+  search: '',
+  setSearch: (val) => set(() => ({ search: val })),
+  clearSearch: () => set({ search: '' }),
+
+  isWaiting: false,
+  setWaiting: (status) => set({ isWaiting: status }),
+
+  loading: {},
+  setLoading: (id) =>
+    set((state) => ({ loading: { ...state.loading, [id]: true } })),
+  doneLoading: () => set({ loading: {} }),
+}));
+
+const useSearchStateHook = () => {
+  const setActiveNote = useActiveNote((state) => state.setActiveNote);
+  const clearSearch = useSearchState((state) => state.clearSearch);
+  const isWaiting = useSearchState((state) => state.isWaiting);
+  const setWaiting = useSearchState((state) => state.setWaiting);
+  const loading = useSearchState((state) => state.loading);
+  const setLoading = useSearchState((state) => state.setLoading);
+  const doneLoading = useSearchState((state) => state.doneLoading);
+  return {
+    setActiveNote,
+    clearSearch,
+    isWaiting,
+    setWaiting,
+    loading,
+    setLoading,
+    doneLoading,
+  };
+};
 
 const RenderNoteItem = ({ title, data, icon, setOpen }: any) => {
-  const setActiveNote = useActiveNote((state) => state.setActiveNote);
-  const [loading, setLoading] = useState<Record<number, boolean>>({});
+  const {
+    setActiveNote,
+    setLoading,
+    clearSearch,
+    loading,
+    doneLoading,
+    setWaiting,
+    isWaiting,
+  } = useSearchStateHook();
   const router = useRouter();
 
   const setNote = (item: NoteTypes) => {
@@ -31,15 +89,18 @@ const RenderNoteItem = ({ title, data, icon, setOpen }: any) => {
     });
   };
 
-  const handlePushRoute = async (item: NoteTypes, index: number) => {
-    setLoading((state) => ({ ...state, [index]: true }));
+  const handlePushRoute = async (item: NoteTypes) => {
+    setLoading(item.id_note);
     setActiveNote(null);
+    setWaiting(true);
     NProgress.start();
     router.replace(`/folders/${item.folder_id}`);
     await setNote(item);
     setOpen(false);
-    setLoading((state) => ({ ...state, [index]: false }));
+    setWaiting(false);
+    doneLoading();
     NProgress.done();
+    clearSearch();
   };
 
   return (
@@ -54,18 +115,18 @@ const RenderNoteItem = ({ title, data, icon, setOpen }: any) => {
             {data?.map((item: NoteTypes, i: number) => (
               <div
                 className={`hover:bg-white/[5%] py-3 rounded-md transition px-3 cursor-pointer relative ${
-                  loading[Number(i)]
-                    ? 'opacity-50 cursor-default pointer-events-none'
+                  loading[item.id_note] && item.deletedAt === null
+                    ? 'opacity-50'
                     : ''
-                } `}
+                } ${isWaiting && 'pointer-events-none cursor-default'} `}
                 key={i}
-                onClick={() => handlePushRoute(item, i)}
+                onClick={() => handlePushRoute(item)}
               >
                 <div className="flex gap-[20px] items-center">
                   {icon}
                   <p className="truncate">{item.name}</p>
                 </div>
-                {loading[Number(i)] === true && (
+                {loading[item.id_note] && item.deletedAt === null && (
                   <div className="absolute inset-y-0 right-0 flex items-center -translate-x-5">
                     <Loader2 className="animate-spin" />
                   </div>
@@ -80,10 +141,17 @@ const RenderNoteItem = ({ title, data, icon, setOpen }: any) => {
 };
 
 const RenderFolderItem = ({ title, data, icon, setOpen }: any) => {
-  const [loading, setLoading] = useState<Record<number, boolean>>({});
-  const setActiveNote = useActiveNote((state) => state.setActiveNote);
-
+  const {
+    setActiveNote,
+    setLoading,
+    clearSearch,
+    loading,
+    doneLoading,
+    setWaiting,
+    isWaiting,
+  } = useSearchStateHook();
   const router = useRouter();
+
   const redirectTo = (item: FolderTypes) => {
     NProgress.start();
     return new Promise((resolve) => {
@@ -96,12 +164,16 @@ const RenderFolderItem = ({ title, data, icon, setOpen }: any) => {
     });
   };
 
-  const handlePushRoute = async (item: FolderTypes, index: number) => {
-    setLoading((state) => ({ ...state, [index]: true }));
+  const handlePushRoute = async (item: FolderTypes) => {
+    setLoading(item.id_folder);
+    setWaiting(true);
     await redirectTo(item);
     setOpen(false);
-    setLoading((state) => ({ ...state, [index]: false }));
+    setWaiting(false);
+    doneLoading();
+    clearSearch();
   };
+
   return (
     <React.Fragment>
       {data?.length !== 0 ? (
@@ -114,18 +186,16 @@ const RenderFolderItem = ({ title, data, icon, setOpen }: any) => {
             {data?.map((item: FolderTypes, i: number) => (
               <div
                 className={`hover:bg-white/[5%] py-3 rounded-md transition px-3 cursor-pointer relative ${
-                  loading[Number(i)]
-                    ? 'opacity-50 cursor-default pointer-events-none'
-                    : ''
-                } `}
+                  loading[item.id_folder] ? 'opacity-50' : ''
+                } ${isWaiting && 'pointer-events-none cursor-default'} `}
                 key={i}
-                onClick={() => handlePushRoute(item, i)}
+                onClick={() => handlePushRoute(item)}
               >
                 <div className="flex gap-[20px] items-center">
                   {icon}
                   <p className="truncate">{item.name}</p>
                 </div>
-                {loading[Number(i)] === true && (
+                {loading[item.id_folder] === true && (
                   <div className="absolute inset-y-0 right-0 flex items-center -translate-x-5">
                     <Loader2 className="animate-spin" />
                   </div>
@@ -140,10 +210,17 @@ const RenderFolderItem = ({ title, data, icon, setOpen }: any) => {
 };
 
 const RenderNoteOnTrash = ({ title, data, icon, setOpen }: any) => {
-  const [loading, setLoading] = useState<Record<number, boolean>>({});
-  const setActiveNote = useActiveNote((state) => state.setActiveNote);
-  const router = useRouter();
+  const {
+    setActiveNote,
+    setLoading,
+    clearSearch,
+    loading,
+    doneLoading,
+    setWaiting,
+    isWaiting,
+  } = useSearchStateHook();
 
+  const router = useRouter();
   const setNote = (item: NoteTypes) => {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -153,14 +230,17 @@ const RenderNoteOnTrash = ({ title, data, icon, setOpen }: any) => {
     });
   };
 
-  const handlePushRoute = async (item: NoteTypes, index: number) => {
+  const handlePushRoute = async (item: NoteTypes) => {
     NProgress.start();
     router.replace(`/trash`);
-    setLoading((state) => ({ ...state, [index]: true }));
+    setLoading(item.id_note as string);
+    setWaiting(true);
     await setNote(item);
     setOpen(false);
     NProgress.done();
-    setLoading((state) => ({ ...state, [index]: false }));
+    doneLoading();
+    clearSearch();
+    setWaiting(false);
   };
   return (
     <React.Fragment>
@@ -174,18 +254,18 @@ const RenderNoteOnTrash = ({ title, data, icon, setOpen }: any) => {
             {data?.map((item: NoteTypes, i: number) => (
               <div
                 className={`hover:bg-white/[5%] py-3 rounded-md transition px-3 cursor-pointer relative ${
-                  loading[Number(i)]
-                    ? 'opacity-50 cursor-default pointer-events-none'
+                  loading[item.id_note] && item.deletedAt !== null
+                    ? 'opacity-50'
                     : ''
-                } `}
+                } ${isWaiting && 'cursor-default pointer-events-none'}`}
                 key={i}
-                onClick={() => handlePushRoute(item, i)}
+                onClick={() => handlePushRoute(item)}
               >
                 <div className="flex gap-[20px] items-center">
                   {icon}
                   <p className="truncate">{item.name}</p>
                 </div>
-                {loading[Number(i)] === true && (
+                {loading[item.id_note] && item.deletedAt !== null && (
                   <div className="absolute inset-y-0 right-0 flex items-center -translate-x-5">
                     <Loader2 className="animate-spin" />
                   </div>
@@ -205,9 +285,11 @@ const RenderNoteOnTrash = ({ title, data, icon, setOpen }: any) => {
 };
 
 function SearchNote() {
-  const [search, setSearch] = useState<string>('');
   const [open, setOpen] = useState<boolean>(false);
   const { folders } = useFolderState();
+  const search = useSearchState((state) => state.search);
+  const setSearch = useSearchState((state) => state.setSearch);
+  const clearSearch = useSearchState((state) => state.clearSearch);
 
   const searchNoteData = useMemo(() => {
     const findNotes = folders.reduce((results, item) => {
@@ -244,15 +326,15 @@ function SearchNote() {
 
   return (
     <>
-      <AlertDialog open={open} onOpenChange={setOpen}>
-        <AlertDialogTrigger asChild>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
           <Button size={'sm'} variant={'ghost'}>
             <FiSearch className="text-[20px]" />
           </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent className="p-0 border max-w-[520px] border-white/[30%] h-[400px] overflow-hidden overflow-y-auto custom-scrollbar">
+        </DialogTrigger>
+        <DialogContent className="p-0 border max-w-[520px] border-white/[30%] h-[400px] overflow-hidden overflow-y-auto custom-scrollbar">
           <div>
-            <div className="flex justify-between items-center px-[20px] sticky top-0 right-0 left-0 bg-background border-b border-white/[20%]">
+            <div className="flex justify-between z-50 items-center px-[20px] sticky top-0 right-0 left-0 bg-background border-b border-white/[20%]">
               <div className="flex items-center gap-4 w-full  py-[20px]">
                 <FiSearch className="text-[20px]" />
                 <input
@@ -261,19 +343,24 @@ function SearchNote() {
                   placeholder="Type a note or folder....."
                   onChange={(e) => setSearch(e.target.value)}
                   maxLength={30}
+                  autoFocus
+                  spellCheck={false}
+                  autoComplete="false"
                 />
               </div>
-              <AlertDialogCancel asChild onClick={() => setSearch('')}>
-                <Button
-                  size={'sm'}
-                  variant={'ghost'}
-                  className="outline-none border-none ring-0 px-[10px] py-[10px]"
-                >
-                  <FiX className="text-[22px]" />
-                </Button>
-              </AlertDialogCancel>
+              <Button
+                size={'sm'}
+                variant={'ghost'}
+                className="outline-none border-none ring-0 px-[10px] py-[10px]"
+                onClick={() => {
+                  clearSearch();
+                  setOpen(false);
+                }}
+              >
+                <FiX className="text-[22px]" />
+              </Button>
             </div>
-            <div className="flex flex-col gap-[20px] mt-[20px]">
+            <div className="flex flex-col gap-[20px] my-[20px]">
               <RenderFolderItem
                 title="Folders"
                 data={searchFolderData}
@@ -294,14 +381,14 @@ function SearchNote() {
               />
             </div>
             {searchNoteData.length === 0 && searchFolderData.length === 0 && (
-              <p className="text-lg flex h-[330px] items-center justify-center truncate">
+              <p className="text-lg flex h-[60%] pt-0 items-center justify-center break-words">
                 <span>No results for</span>
                 <span className="ml-2 font-bold text-white">{search}</span>
               </p>
             )}
           </div>
-        </AlertDialogContent>
-      </AlertDialog>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
