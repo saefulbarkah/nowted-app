8baf72d02312c7f35e20542c3b548f4dfbeb1409
saefulbarkah <saefulbarkah03@gmail.com>
2023-07-05 09:28:53 +0700

    fix: placeholder editor appears when content is not empty

--- src/components/ui/Editor/EditorTipTap.tsx
+++ src/components/ui/Editor/EditorTipTap.tsx
@@ -1,8 +1,6 @@
 'use client';
-import { BubbleMenu, Editor, EditorContent } from '@tiptap/react';
+import { Editor, EditorContent } from '@tiptap/react';
 import './editor.css';
-import ToggleMenus from './Menu/ToggleMenus';
-import LinkMenu from './Menu/LinkMenu';
 
 export const EditorTipTap = ({ editor }: { editor: Editor | null }) => {
   if (!editor) return null;
