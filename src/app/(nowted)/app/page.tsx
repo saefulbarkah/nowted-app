5fabf822812ce2019df20476b30e72000ea9d805
saefulbarkah <saefulbarkah03@gmail.com>
2023-07-26 23:27:07 +0700

    remove: pwa

--- src/app/(nowted)/app/page.tsx
+++ src/app/(nowted)/app/page.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
 import { Metadata } from 'next';
 import { FolderOpen } from 'lucide-react';
-const APP_NAME = 'Nowted APP';
 export const metadata: Metadata = {
-  applicationName: APP_NAME,
-  appleWebApp: {
-    capable: true,
-    title: APP_NAME,
-    statusBarStyle: 'default',
-  },
-
-  themeColor: '#FFFFFF',
-  viewport:
-    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
-  manifest: '/manifest.json',
-  icons: [
-    { rel: 'apple-touch-icon', url: '/icons/apple-touch-icon.png' },
-    { rel: 'shortcut icon', url: '/favicon.ico' },
-  ],
+  title: 'Nowted APP',
   keywords: [
     'nowted-app',
     'notes applications',
