7f30f1ebbcdf34dd2ab5192f63d70473a5022238
XopH <saefulbarkah650@gmail.com>
2025-03-15 20:28:28 +0700

    chore: Formatting DeleteFolder.tsx

--- src/components/Sidebar/FolderMenu/DeleteFolder.tsx
+++ src/components/Sidebar/FolderMenu/DeleteFolder.tsx
@@ -1,29 +1,15 @@
-import Dvider from '@/components/ui/Dvider';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-} from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-} from '@/components/ui/dialog';
-import { useToast } from '@/components/ui/use-toast';
-import useFolderState from '@/hooks/useFolderState';
-import { useRecentStore } from '@/store/useRecentStore';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import Dvider from "@/components/ui/Dvider";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader } from "@/components/ui/dialog";
+import { useToast } from "@/components/ui/use-toast";
+import useFolderState from "@/hooks/useFolderState";
+import { useRecentStore } from "@/store/useRecentStore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DialogDelete = () => {
   const { toast } = useToast();
-  const { isOpenDialogDelete, setDialogDelete, deleteData, removeFolder } =
-    useFolderState();
+  const { isOpenDialogDelete, setDialogDelete, deleteData, removeFolder } = useFolderState();
   const [isLoading, setIsLoading] = useState(false);
   const removeRecents = useRecentStore((state) => state.removeRecents);
   const recents = useRecentStore((state) => state.recents);
@@ -31,21 +17,19 @@ const DialogDelete = () => {
 
   const handleDeleteFolder = () => {
     recents.map((item) =>
-      item.folder_id === deleteData.id_folder
-        ? removeRecents({ id_note: item.id_note })
-        : null
+      item.folder_id === deleteData.id_folder ? removeRecents({ id_note: item.id_note }) : null
     );
     setIsLoading(true);
     setTimeout(() => {
       toast({
-        title: 'Success',
-        description: 'Delete folder successfully',
-        variant: 'success',
+        title: "Success",
+        description: "Delete folder successfully",
+        variant: "success",
       });
       setDialogDelete(false);
       setIsLoading(false);
       removeFolder({ id_folder: deleteData.id_folder });
-      router.replace('/app');
+      router.replace("/app");
     }, 1000);
   };
 
@@ -58,22 +42,18 @@ const DialogDelete = () => {
         </DialogHeader>
         <p>
           Are you sure you want to delete
-          <span className="font-bold"> {deleteData.name} </span> folder?. This
-          will also delete all notes
+          <span className="font-bold"> {deleteData.name} </span> folder?. This will also delete all
+          notes
         </p>
         <DialogFooter>
-          <Button
-            onClick={() => setDialogDelete(false)}
-            size={'sm'}
-            variant={'secondary'}
-          >
+          <Button onClick={() => setDialogDelete(false)} size={"sm"} variant={"secondary"}>
             Cancel
           </Button>
           <Button
             isLoading={isLoading}
             onClick={() => handleDeleteFolder()}
-            size={'sm'}
-            variant={'destructive'}
+            size={"sm"}
+            variant={"destructive"}
           >
             Yes, delete it
           </Button>
